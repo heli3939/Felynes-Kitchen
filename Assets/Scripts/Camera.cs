39d983d4aa24d80981efc4a71d6935b6dc3981c8
SeL033 <1499818955@qq.com>
2025-09-20 16:37:33 +1000

    add map boundary limit for camera movement

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,6 +5,9 @@ public class CameraFollow : MonoBehaviour
     private Transform player;
     private float posY;
     private float posZ;
+    private float currX;
+    private float maxLeft = -8.181f;
+    private float maxRight = 7.85f;
     void Start()
     {
         GameObject getPlayer = GameObject.FindGameObjectWithTag("Player");
@@ -15,11 +18,30 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         if (!player) return;
-
-        transform.position = new Vector3(
-            player.position.x,
-            posY,
-            posZ
-        );
+        currX = player.position.x;
+        if (currX > maxLeft && currX < maxRight)
+        {
+            transform.position = new Vector3(
+                player.position.x,
+                posY,
+                posZ
+            );
+        }
+        else if (currX <= maxLeft)
+        {
+            transform.position = new Vector3(
+                maxLeft,
+                posY,
+                posZ
+            );
+        }
+        else
+        {
+            transform.position = new Vector3(
+                maxRight,
+                posY,
+                posZ
+            );
+        }
     }
 }
