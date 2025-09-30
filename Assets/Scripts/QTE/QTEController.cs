2cfa94545424709671de6c827118ace74265c402
Difei-Li <difei.li1@student.unimelb.edu.au>
2025-09-30 14:58:47 +1000

    Initial implementation of QTE logic

---
+++ Assets/Scripts/QTE/QTEController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QTEUIController : MonoBehaviour
+{
+    [Header("Refs")]
+    public RectTransform circleGroup;     
+    public RectTransform indicator;      
+    public Image successZoneImage;        
+
+    [Header("Config")]
+    public int totalChecks = 3;           // check 3 times for once
+    public float successArcDegrees = 60f; 
+    public float rotateSpeed = 180f;      
+    public KeyCode confirmKey = KeyCode.Space;
+    public bool autoStartOnEnable = true; 
+
+    [Header("State (read-only)")]
+    [SerializeField] private bool isRunning = false;
+    [SerializeField] private int currentCheck = 0;
+
+    public Action<bool[]> OnQTEFinished;  
+
+    private bool[] results;
+
+    private void OnEnable()
+    {
+        if (autoStartOnEnable) StartQTE();
+    }
+
+    private void OnDisable()
+    {
+        isRunning = false;
+    }
+
+    private void Update()
+    {
+        if (!isRunning) return;
+
+        float z = indicator.localEulerAngles.z;
+        z = Mathf.Repeat(z - rotateSpeed * Time.deltaTime, 360f);
+        indicator.localEulerAngles = new Vector3(0f, 0f, z);
+
+        if (Input.GetKeyDown(confirmKey))
+        {
+            bool hit = EvaluateHit();
+            results[currentCheck] = hit;
+
+            currentCheck++;
+            if (currentCheck >= totalChecks)
+            {
+                Finish();
+            }
+            else
+            {
+                RandomizeCircleGroup();
+            }
+        }
+    }
+
+    public void StartQTE()
+    {
+
+        successZoneImage.type = Image.Type.Filled;
+        successZoneImage.fillMethod = Image.FillMethod.Radial360;
+        successZoneImage.fillAmount = Mathf.Clamp01(successArcDegrees / 360f);
+
+
+        results = new bool[totalChecks];
+        currentCheck = 0;
+        isRunning = true;
+
+        indicator.localEulerAngles = Vector3.zero;
+
+        RandomizeCircleGroup();
+    }
+
+    public void StopQTE() 
+    {
+        isRunning = false;
+    }
+
+    private void Finish()
+    {
+        isRunning = false;
+        OnQTEFinished?.Invoke(results);
+    }
+
+    private void RandomizeCircleGroup()
+    {
+        float a = UnityEngine.Random.Range(0f, 360f);
+        circleGroup.localEulerAngles = new Vector3(0f, 0f, a);
+    }
+
+    private bool EvaluateHit()
+    {
+
+        float indicatorWorld = Mathf.Repeat(indicator.eulerAngles.z, 360f);
+        float zoneCenterWorld = Mathf.Repeat(successZoneImage.rectTransform.eulerAngles.z, 360f);
+
+        float half = successArcDegrees * 0.5f;
+        float diff = Mathf.Abs(Mathf.DeltaAngle(indicatorWorld, zoneCenterWorld));
+        return diff <= half;
+    }
+}
---
+++ Assets/Scripts/QTE/QTEManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class QTEManager : MonoBehaviour
+{
+    [Header("Cameras")]
+    public GameObject tpCamera;  
+    public GameObject fpCamera;   
+
+    [Header("QTE UI")]
+    public GameObject qteCanvas;        // QTE Canvas
+    public QTEUIController qteController;
+
+    private void Awake()
+    {
+        if (qteController != null)
+            qteController.OnQTEFinished += HandleFinished;
+    }
+
+    private void OnDestroy()
+    {
+        if (qteController != null)
+            qteController.OnQTEFinished -= HandleFinished;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartQTE();
+        }
+    }
+
+    public void StartQTE()
+    {
+        if (tpCamera != null) tpCamera.SetActive(false);
+        if (fpCamera != null) fpCamera.SetActive(true);
+
+        if (qteCanvas != null) qteCanvas.SetActive(true);
+    }
+
+    private void HandleFinished(bool[] results)
+    {
+        // Statistical results
+        bool allSuccess = true;
+        foreach (var r in results) if (!r) { allSuccess = false; break; }
+
+        EndQTE();
+    }
+
+    public void EndQTE()
+    {
+        // close QTE
+        if (qteController != null) qteController.StopQTE();
+        if (qteCanvas != null) qteCanvas.SetActive(false);
+
+        // back to main camera
+        if (fpCamera != null) fpCamera.SetActive(false);
+        if (tpCamera != null) tpCamera.SetActive(true);
+    }
+}
